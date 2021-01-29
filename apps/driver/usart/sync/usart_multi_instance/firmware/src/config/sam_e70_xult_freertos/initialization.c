9834bb0d5553fe208a8c36fea6d0d4e4f6153146
Amit Raddi <amit.raddi@microchip.com>
2021-01-29 18:21:19 +0530

    [DRV_USART] Regenerated demo for driver_usart_sync_usart_multi_instance_sam_e70_xult_freertos
    MH3-50606
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/usart/sync/usart_multi_instance/firmware/src/config/sam_e70_xult_freertos/initialization.c
+++ apps/driver/usart/sync/usart_multi_instance/firmware/src/config/sam_e70_xult_freertos/initialization.c
@@ -76,7 +76,7 @@ const DRV_USART_PLIB_INTERFACE drvUsart1PlibAPI = {
     .read = (DRV_USART_PLIB_READ)USART1_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)USART1_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)USART1_ReadCountGet,
-	.readAbort = (DRV_USART_PLIB_READ_ABORT)USART1_ReadAbort,
+    .readAbort = (DRV_USART_PLIB_READ_ABORT)USART1_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)USART1_WriteCallbackRegister,
     .write = (DRV_USART_PLIB_WRITE)USART1_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)USART1_WriteIsBusy,
@@ -116,6 +116,8 @@ const DRV_USART_INIT drvUsart1InitData =
     .remapStopBits = drvUsart1remapStopBits,
 
     .remapError = drvUsart1remapError,
+
+    .dataWidth = DRV_USART_DATA_8_BIT,
 };
 
 // </editor-fold>
@@ -129,7 +131,7 @@ const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
     .read = (DRV_USART_PLIB_READ)USART0_Read,
     .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)USART0_ReadIsBusy,
     .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)USART0_ReadCountGet,
-	.readAbort = (DRV_USART_PLIB_READ_ABORT)USART0_ReadAbort,
+    .readAbort = (DRV_USART_PLIB_READ_ABORT)USART0_ReadAbort,
     .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)USART0_WriteCallbackRegister,
     .write = (DRV_USART_PLIB_WRITE)USART0_Write,
     .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)USART0_WriteIsBusy,
@@ -169,6 +171,8 @@ const DRV_USART_INIT drvUsart0InitData =
     .remapStopBits = drvUsart0remapStopBits,
 
     .remapError = drvUsart0remapError,
+
+    .dataWidth = DRV_USART_DATA_8_BIT,
 };
 
 // </editor-fold>
@@ -217,6 +221,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
 
     EFC_Initialize();
   
