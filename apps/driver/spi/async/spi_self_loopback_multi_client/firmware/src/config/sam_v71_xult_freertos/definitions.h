2d153ff937d288820a4c27051f2cee042efacb02
Amit Raddi <amit.raddi@microchip.com>
2021-01-29 18:09:03 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_sam_v71_xult_freertos
    MH3-50587
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_v71_xult_freertos/definitions.h
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_v71_xult_freertos/definitions.h
@@ -51,7 +51,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/spi/plib_spi0.h"
+#include "peripheral/spi/spi_master/plib_spi0_master.h"
 #include "peripheral/efc/plib_efc.h"
 #include "FreeRTOS.h"
 #include "task.h"
@@ -76,6 +76,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 300000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -169,22 +172,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* SPI0 Driver Object */
