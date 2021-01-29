2d61962df9849221e222b977c8eb617996480dd7
Amit Raddi <amit.raddi@microchip.com>
2021-01-29 17:58:19 +0530

    [DRV_I2C_EEPROM] Regenerated demo for driver_i2c_eeprom_at24_at24_eeprom_read_write_sam_e70_xult_freertos
    MH3-50569
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -53,7 +53,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
-#include "peripheral/twihs/plib_twihs0.h"
+#include "peripheral/twihs/master/plib_twihs0_master.h"
 #include "peripheral/efc/plib_efc.h"
 #include "FreeRTOS.h"
 #include "task.h"
@@ -73,6 +73,9 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* CPU clock frequency */
+#define CPU_CLOCK_FREQUENCY 300000000
 
 // *****************************************************************************
 // *****************************************************************************
@@ -166,22 +169,22 @@ void SYS_Tasks ( void );
 // Section: Type Definitions
 // *****************************************************************************
 // *****************************************************************************
-    
+
 // *****************************************************************************
 /* System Objects
-        
+
 Summary:
     Structure holding the system's object handles
-        
+
 Description:
     This structure contains the object handles for all objects in the
     MPLAB Harmony project's system configuration.
-        
+
 Remarks:
     These handles are returned from the "Initialize" functions for each module
     and must be passed into the "Tasks" function for each module.
 */
-        
+
 typedef struct
 {
     /* AT24 Driver Object */
