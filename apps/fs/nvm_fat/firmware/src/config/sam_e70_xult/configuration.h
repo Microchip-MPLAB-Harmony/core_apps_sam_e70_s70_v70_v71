e66369af54ccb69771e071061b14b34d419072f3
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-18 14:58:18 +0530

    [SYS_FS] Regenerated demo for fs_nvm_fat_sam_e70_xult
    MH3-56445
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/nvm_fat/firmware/src/config/sam_e70_xult/configuration.h
+++ apps/fs/nvm_fat/firmware/src/config/sam_e70_xult/configuration.h
@@ -56,7 +56,7 @@
 */
 
 #include "user.h"
-#include "toolchain_specifics.h"
+#include "device.h"
 
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
@@ -106,6 +106,7 @@ extern "C" {
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: Driver Configuration
@@ -118,6 +119,7 @@ extern "C" {
 #define DRV_MEMORY_BUFFER_QUEUE_SIZE_IDX0    1
 #define DRV_MEMORY_DEVICE_START_ADDRESS      0x500000
 #define DRV_MEMORY_DEVICE_MEDIA_SIZE         1024UL
+#define DRV_MEMORY_DEVICE_MEDIA_SIZE_BYTES   (DRV_MEMORY_DEVICE_MEDIA_SIZE * 1024)
 #define DRV_MEMORY_DEVICE_PROGRAM_SIZE       512
 #define DRV_MEMORY_DEVICE_ERASE_SIZE         8192
 
