e82c68b4800cb433eb121c5d5e33b0124e8dcf39
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-18 15:36:04 +0530

    [SYS_FS] Regenerated demo for fs_sdspi_fat_sam_e70_xult
    MH3-56587
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/sdspi_fat/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/fs/sdspi_fat/firmware/src/config/sam_e70_xult/initialization.c
@@ -87,7 +87,7 @@ const DRV_SDSPI_PLIB_INTERFACE drvSDSPI0PlibAPI = {
     .read = (DRV_SDSPI_PLIB_READ)SPI0_Read,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SDSPI_PLIB_IS_BUSY)SPI0_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SPI0_IsTransmitterBusy,
 
     .transferSetup = (DRV_SDSPI_PLIB_SETUP)SPI0_TransferSetup,
 
@@ -211,13 +211,15 @@ const SYS_FS_FUNCTIONS FatFsFunctions =
 };
 
 
+
 const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 {
     {
         .nativeFileSystemType = FAT,
         .nativeFileSystemFunctions = &FatFsFunctions
-    }
+    },
 };
+
 
 // </editor-fold>
 
