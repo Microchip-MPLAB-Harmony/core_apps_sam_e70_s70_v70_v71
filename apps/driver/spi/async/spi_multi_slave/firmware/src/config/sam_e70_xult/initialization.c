6f88f5210701fcaddb8542a54c666df1bc9ae681
Vishal Gandhi <vishal.gandhi@microchip.com>
2021-08-18 14:23:03 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_multi_slave_sam_e70_xult
    MH3-56417
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_e70_xult/initialization.c
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_e70_xult/initialization.c
@@ -84,7 +84,7 @@ const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
     .writeRead = (DRV_SPI_PLIB_WRITE_READ)SPI0_WriteRead,
 
     /* SPI PLIB Transfer Status function */
-    .isBusy = (DRV_SPI_PLIB_IS_BUSY)SPI0_IsBusy,
+    .isTransmitterBusy = (DRV_SPI_PLIB_TRANSMITTER_IS_BUSY)SPI0_IsTransmitterBusy,
 
     /* SPI PLIB Callback Register */
     .callbackRegister = (DRV_SPI_PLIB_CALLBACK_REGISTER)SPI0_CallbackRegister,
