8eae3c7c65b1b61c23691142f2c792fa3c615188
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-16 03:47:45 +0530

    [FREERTOS] Regenerated demo for rtos_freertos_task_notification_freertos_sam_v71_xult
    MH3-65513
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/rtos/freertos/task_notification_freertos/firmware/src/config/sam_v71_xult/peripheral/nvic/plib_nvic.c
+++ apps/rtos/freertos/task_notification_freertos/firmware/src/config/sam_v71_xult/peripheral/nvic/plib_nvic.c
@@ -61,7 +61,13 @@ void NVIC_Initialize( void )
      * from within the "Interrupt Manager" of MHC. */
     NVIC_SetPriority(SysTick_IRQn, 7);
 
+    /* Enable Usage fault */
+    SCB->SHCSR |= (SCB_SHCSR_USGFAULTENA_Msk);
+    /* Trap divide by zero */
+    SCB->CCR   |= SCB_CCR_DIV_0_TRP_Msk;
 
+    /* Enable Bus fault */
+    SCB->SHCSR |= (SCB_SHCSR_BUSFAULTENA_Msk);
 
 }
 
@@ -73,9 +79,7 @@ void NVIC_INT_Enable( void )
 
 bool NVIC_INT_Disable( void )
 {
-    bool processorStatus;
-
-    processorStatus = (bool) (__get_PRIMASK() == 0);
+    bool processorStatus = (__get_PRIMASK() == 0U);
 
     __disable_irq();
     __DMB();
