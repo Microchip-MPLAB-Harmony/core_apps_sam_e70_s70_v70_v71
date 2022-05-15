57dc1cb7b4f167812fd74a9ccba7fbc2a76c4866
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-16 01:13:01 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_async_i2c_eeprom_sam_v71_xult_freertos
    MH3-65314
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_v71_xult_freertos/definitions.h
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_v71_xult_freertos/definitions.h
@@ -53,14 +53,14 @@
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/twihs/master/plib_twihs0_master.h"
 #include "peripheral/efc/plib_efc.h"
+#include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
-#include "bsp/bsp.h"
-#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
+#include "driver/i2c/drv_i2c.h"
 #include "app.h"
 
 
