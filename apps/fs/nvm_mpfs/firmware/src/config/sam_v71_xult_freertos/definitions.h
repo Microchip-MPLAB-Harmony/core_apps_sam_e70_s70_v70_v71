94a44e0fa9c3f1ad7ff06185827d2a389de7c441
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-16 02:55:04 +0530

    [SYS_FS] Regenerated demo for fs_nvm_mpfs_sam_v71_xult_freertos
    MH3-65492
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/fs/nvm_mpfs/firmware/src/config/sam_v71_xult_freertos/definitions.h
+++ apps/fs/nvm_mpfs/firmware/src/config/sam_v71_xult_freertos/definitions.h
@@ -62,11 +62,11 @@
 #include "FreeRTOS.h"
 #include "task.h"
 #include "bsp/bsp.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
