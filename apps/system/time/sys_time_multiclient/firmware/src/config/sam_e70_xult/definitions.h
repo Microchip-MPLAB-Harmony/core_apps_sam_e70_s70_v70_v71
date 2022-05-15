da19930981327a758d34b097b2e401a6da0c467e
Vishal Gandhi <vishal.gandhi@microchip.com>
2022-05-16 04:01:02 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_sam_e70_xult
    MH3-65519
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_e70_xult/definitions.h
@@ -56,11 +56,11 @@
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "peripheral/usart/plib_usart1.h"
+#include "system/time/sys_time.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
 #include "system/debug/sys_debug.h"
-#include "system/time/sys_time.h"
 #include "app.h"
 
 
