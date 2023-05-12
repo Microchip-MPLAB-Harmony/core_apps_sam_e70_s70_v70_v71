634502cc345a3e82fc345a1f8f4091bc95d712f7
Vishal Gandhi <vishal.gandhi@microchip.com>
2023-05-12 11:43:47 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_multi_instance_sam_e70_xult_freertos
    MH3-76752

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -54,9 +54,9 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/efc/plib_efc.h"
-#include "peripheral/usart/plib_usart1.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "peripheral/usart/plib_usart1.h"
 #include "peripheral/usart/plib_usart0.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
@@ -75,6 +75,12 @@ extern "C" {
 
 #endif
 // DOM-IGNORE-END
+
+/* Device Information */
+#define DEVICE_NAME			 "ATSAME70Q21B"
+#define DEVICE_ARCH			 "CORTEX-M7"
+#define DEVICE_FAMILY		 "SAME"
+#define DEVICE_SERIES		 "SAME70"
 
 /* CPU clock frequency */
 #define CPU_CLOCK_FREQUENCY 300000000
